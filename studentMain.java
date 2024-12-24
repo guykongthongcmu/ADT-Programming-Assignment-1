526b4c15f4c80a3a2815027b8c814eb8988dc9ec
Guy Kongthong <guykongthongcmu@gmail.com>
2024-12-24 21:18:46 +0700

    Update studentMain.java

--- studentMain.java
+++ studentMain.java
@@ -35,56 +35,55 @@ public class studentMain {
         
         Scanner input = new Scanner(System.in);
         Vector<String> temp = new Vector<String>();
+        String userInput = "";
         String choice = "";
         String fileName = "";
-        do {
-            System.out.println("What would you like to do with list of student's names? \n 1. Input -n to sort Students by their ID \n 2. Input -f to sort Students by their first name \n 3. Input -l to sort Students by their last name \n 4. Input -s to Search a Student's Information by their first name \n 5. -e to Exit the Program");
-            choice = input.nextLine();
-    
-            switch (choice.toLowerCase()) {
-                case "-n":
-                    System.out.println("Please input file: ");
-                    fileName = input.nextLine();
-                    for (int i = 0; i < fileReading(fileName).size(); i++) {
-                        temp.add(fileReading(fileName).elementAt(i).getSID());
-                    }
-                    bubbleSort(fileReading(fileName), temp);
-                    break;
-                case "-f":
-                    System.out.println("Please input file: ");
-                    fileName = input.nextLine();
-                    for (int i = 0; i < fileReading(fileName).size(); i++) {
-                        temp.add(fileReading(fileName).elementAt(i).getFirstName());
-                    }
-                    bubbleSort(fileReading(fileName), temp);
-                    break;
-                case "-l":
-                    System.out.println("Please input file: ");
-                    fileName = input.nextLine();
-                    for (int i = 0; i < fileReading(fileName).size(); i++) {
-                        temp.add(fileReading(fileName).elementAt(i).getLastName());
-                    }
-                    bubbleSort(fileReading(fileName), temp);
-                    break;
-                case "-s":
-                    System.out.println("Please input the first name you want to search");
-                    String searchFN = input.nextLine();
-                    int searchResult = linearSearch(fileReading(fileName), searchFN);
-                    if (searchResult != -1) {
-                        System.out.println("The name " + searchFN + " was found at index " + searchResult);
-                    } else {
-                        System.out.println("The name " + searchFN + " is either spelt incorrectly or not in the database");
-                    }
-                    break;
-                case "-e":
-                    System.out.println("Exiting Program!");
-                    break;
-                default:
-                    System.out.println("Invalid input! Please try again");
-                    break;
-            }
-            System.out.println();
-        } while (!choice.toLowerCase().equals("-e"));
+        String searchFN = "";
+
+
+        userInput = input.nextLine();
+        StringTokenizer userInputs = new StringTokenizer(userInput.trim(), " ");
+        fileName = userInputs.nextToken();
+        choice = userInputs.nextToken();
+        if (choice.equals("-s")) {
+            searchFN = userInputs.nextToken();
+        }
+
+        switch (choice.toLowerCase()) {
+            case "-n":
+                for (int i = 0; i < fileReading(fileName).size(); i++) {
+                    temp.add(fileReading(fileName).elementAt(i).getSID());
+                }
+                bubbleSort(fileReading(fileName), temp);
+                break;
+            case "-f":
+                for (int i = 0; i < fileReading(fileName).size(); i++) {
+                    temp.add(fileReading(fileName).elementAt(i).getFirstName());
+                }
+                bubbleSort(fileReading(fileName), temp);
+                break;
+            case "-l":
+                for (int i = 0; i < fileReading(fileName).size(); i++) {
+                    temp.add(fileReading(fileName).elementAt(i).getLastName());
+                }
+                bubbleSort(fileReading(fileName), temp);
+                break;
+            case "-s":
+                int searchResult = linearSearch(fileReading(fileName), searchFN);
+                if (searchResult != -1) {
+                    System.out.println("The name " + searchFN + " was found at index " + searchResult);
+                } else {
+                    System.out.println("The name " + searchFN + " is either spelt incorrectly or not in the database");
+                }
+                break;
+            case "-e":
+                System.out.println("Exiting Program!");
+                break;
+            default:
+                System.out.println("Invalid input! Please try again");
+                break;
+        }
+        System.out.println();
     }
 
     public static void bubbleSort(Vector<studentClass> vector, Vector<String> temp) {
